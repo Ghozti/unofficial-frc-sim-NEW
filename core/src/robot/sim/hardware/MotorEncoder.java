e8821b7499ffb63215f7690c629a0a40a7b1f164
Ghozti <ybalderas1005@gmail.com>
2021-07-27 14:46:57 -0400

    - added fields to MotorEncoder

--- core/src/robot/sim/hardware/MotorEncoder.java
+++ core/src/robot/sim/hardware/MotorEncoder.java
@@ -2,11 +2,19 @@ package robot.sim.hardware;
 
 public class MotorEncoder {
 
-    public MotorEncoder(Motor assignedMotor, float id){
+    //fields
+    Motor assignedMotor;
+    float id;
+    MotorEncoder master;
 
+    public MotorEncoder(Motor assignedMotor, float id){
+        this.assignedMotor = assignedMotor;
+        this.id = id;
     }
 
     public MotorEncoder(Motor assignedMotor, float id,MotorEncoder masterMotorEncoder){
-
+        this.assignedMotor = assignedMotor;
+        this.id = id;
+        master = masterMotorEncoder;
     }
 }
