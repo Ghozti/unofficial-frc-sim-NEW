dccb808a1ec76894abc05e915b4c88dd1e52f8d4
Ghozti <ybalderas1005@gmail.com>
2021-07-27 14:42:24 -0400

    - added MotorEncoder class constructors

--- core/src/robot/sim/hardware/MotorEncoder.java
+++ core/src/robot/sim/hardware/MotorEncoder.java
@@ -1,4 +1,12 @@
 package robot.sim.hardware;
 
 public class MotorEncoder {
+
+    public MotorEncoder(Motor assignedMotor, float id){
+
+    }
+
+    public MotorEncoder(Motor assignedMotor, float id,MotorEncoder masterMotorEncoder){
+
+    }
 }
