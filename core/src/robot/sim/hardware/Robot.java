8c295bf620870c6b18bf336bde7cffacaadbf423
Ghozti <ybalderas1005@gmail.com>
2021-07-28 14:06:56 -0400

    - worked on Robot.java

--- core/src/robot/sim/hardware/Robot.java
+++ core/src/robot/sim/hardware/Robot.java
@@ -2,11 +2,35 @@ package robot.sim.hardware;
 
 public class Robot {
 
+    float currentSpeed;
     String name;
     Chassis chassis;
     float width = 18f, length = 30f;
 
     public Robot(String name){
         this.name = name;
+        currentSpeed = 0;
+    }
+
+    public void buildChassis(Chassis chassis){
+        this.chassis = chassis;
+
+        /*
+                                  FRONT
+                            ________________
+                     LEFT   |               |   RIGHT
+                          0 |               | 0
+                          1 |     robot     | 1
+                          2 |               | 2
+                            |               |
+                            -----------------
+                                  BACK
+         */
+
+        chassis.addLeftSideEncoder(new MotorEncoder(new Motor(),0));
+    }
+
+    public void driveForward(){
+
     }
 }
