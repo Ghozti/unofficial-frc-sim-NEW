8e586eb06fe9e43999738deaa317f3a38603a545
Ghozti <ybalderas1005@gmail.com>
2021-07-28 01:04:43 -0400

    - added methods to MotorEncoder

--- core/src/robot/sim/hardware/MotorEncoder.java
+++ core/src/robot/sim/hardware/MotorEncoder.java
@@ -3,9 +3,11 @@ package robot.sim.hardware;
 public class MotorEncoder {
 
     //fields
-    Motor assignedMotor;
-    float id;
-    MotorEncoder master;
+    private Motor assignedMotor;
+    private float id;
+    private MotorEncoder master;
+
+    /*CONSTRUCTORS*/
 
     public MotorEncoder(Motor assignedMotor, float id){
         this.assignedMotor = assignedMotor;
@@ -16,5 +18,29 @@ public class MotorEncoder {
         this.assignedMotor = assignedMotor;
         this.id = id;
         master = masterMotorEncoder;
+    }
+
+    /*GETTERS*/
+
+    public Motor getAssignedMotor(){
+        return assignedMotor;
+    }
+
+    public float getId(){
+        return id;
+    }
+
+    public MotorEncoder getMaster(){
+        return master;
+    }
+
+    /*SETTERS*/
+
+    public void setSpeed(float speed){
+        assignedMotor.setCurrentPower(1);
+    }
+
+    public void setSpeed(MotorEncoder encoder){
+        assignedMotor.setCurrentPower(encoder.getAssignedMotor().getCurrentPower());
     }
 }
