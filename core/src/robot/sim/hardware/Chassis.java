f088734cb86ba69ab4af5487054395e5132b838c
Ghozti <ybalderas1005@gmail.com>
2021-07-28 01:15:59 -0400

    - added Chassis methods

--- core/src/robot/sim/hardware/Chassis.java
+++ core/src/robot/sim/hardware/Chassis.java
@@ -4,7 +4,24 @@ import java.util.ArrayList;
 
 public class Chassis {
 
-    public Chassis(String chassisType){
+    //FIELDS
+    String chassisType;
 
+    ArrayList<MotorEncoder> rightSideEncoders = new ArrayList<>();
+    ArrayList<MotorEncoder> leftSideEncoders = new ArrayList<>();
+
+    //CONSTRUCTOR
+    public Chassis(String chassisType){
+        this.chassisType = chassisType;
     }
+
+    //METHODS
+    public void addRightEncoders(MotorEncoder encoder){
+        rightSideEncoders.add(encoder);
+    }
+
+    public void addLeftSideEncoder(MotorEncoder encoder){
+        leftSideEncoders.add(encoder);
+    }
+
 }
