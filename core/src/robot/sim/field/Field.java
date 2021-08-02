6167f3b5acdef9cf185a587c05f71e008dcc716b
Ghozti <ybalderas1005@gmail.com>
2021-08-02 12:48:02 -0400

    - added rectangle fields

--- core/src/robot/sim/field/Field.java
+++ core/src/robot/sim/field/Field.java
@@ -1,4 +1,16 @@
 package robot.sim.field;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import java.awt.*;
+
 public class Field {
+
+    //graphics
+    private TextureRegion texture;
+    float width,height;
+
+    //boundingboxes
+    private Rectangle pole0, pole1,pole2,pole3;
+
 }
