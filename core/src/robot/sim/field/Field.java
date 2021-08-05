541feeb2d6b9ec03e4874ac5ab0866f1dc86eea4
Ghozti <ybalderas1005@gmail.com>
2021-08-05 17:23:47 -0400

    - added the field texture

--- core/src/robot/sim/field/Field.java
+++ core/src/robot/sim/field/Field.java
@@ -2,6 +2,7 @@ package robot.sim.field;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import robot.sim.hardware.Robot;
 
 import java.awt.*;
 
@@ -18,13 +19,12 @@ public class Field {
     //field robot
     robot.sim.hardware.Robot robot;
     
-    
     public Field(Robot robot){
         this.robot = robot;
     }
 
     public void render(){
         batch.draw(robot.getTexture(),robot.getX(),robot.getY(),robot.getWidth(),robot.getLength());//draws robot
+        batch.draw(texture,0,0,1920,1080);//draws field
     }
-
 }
