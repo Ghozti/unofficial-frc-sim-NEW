c55ccff0e6efb77ebce146d629d2d733a14b73f4
Ghozti <ybalderas1005@gmail.com>
2021-08-06 01:20:48 -0400

    - reworked Game and GameLauncher

--- core/src/robot/sim/Game.java
+++ core/src/robot/sim/Game.java
@@ -1,15 +1,18 @@
 package robot.sim;
+
+import robot.sim.game.GameLauncher;
+
 public class Game extends com.badlogic.gdx.Game {
 
 
 	@Override
 	public void create() {
-
+		setScreen(new GameLauncher());
 	}
 
 	@Override
 	public void render() {
-
+		super.render();
 	}
 
 	@Override
--- core/src/robot/sim/game/GameLauncher.java
+++ core/src/robot/sim/game/GameLauncher.java
@@ -1,4 +1,44 @@
 package robot.sim.game;
 
-public class GameLauncher {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+
+public class GameLauncher implements Screen {
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(.128f,.128f,.128f,.1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
