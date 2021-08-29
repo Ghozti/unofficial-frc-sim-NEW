51255d4af1b58dbaffb33f9d7866c9d827e91a20
Ghozti <ybalderas1005@gmail.com>
2021-08-28 21:50:33 -0400

    - added Shooter Constructor and fields

--- core/src/robot/sim/hardware/Shooter.java
+++ core/src/robot/sim/hardware/Shooter.java
@@ -1,10 +1,20 @@
 package robot.sim.hardware;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
 public class Shooter {
 
-    float width, height, x , y;
+    float width, length, x , y;
+    Texture projectile;
+    Rectangle hitbox;
 
-    public Shooter(float width, float length, float x, float y){
-
+    public Shooter(float width, float length, float x, float y, Texture projectile){
+        this.width = width;
+        this.length = length;
+        this.x = x;
+        this.y = y;
+        this.projectile = projectile;
+        hitbox = new Rectangle(x,y,width,length);
     }
 }
