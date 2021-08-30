48349bff4ff09826f47401c26770146a492b57dc
Ghozti <ybalderas1005@gmail.com>
2021-08-30 15:02:55 -0400

    - filled in Projectile

--- core/src/robot/sim/hardware/Projectile.java
+++ core/src/robot/sim/hardware/Projectile.java
@@ -1,9 +1,25 @@
 package robot.sim.hardware;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
 public class Projectile {
 
+    private float x, y, width, height;//dimensions and position
+    private Texture texture;
+    private Rectangle hitbox;
+
+    public float getX(){return x;}
+    public float getY(){return y;}
+    public float getWidth(){return width;}
+    public float getHeight(){return height;}
+    public Rectangle getHitbox(){return hitbox;}
+
+    public void display(Batch batch){
+        batch.begin();
+        batch.draw(texture,x,y,width,height);
+        batch.end();
+    }
 }
