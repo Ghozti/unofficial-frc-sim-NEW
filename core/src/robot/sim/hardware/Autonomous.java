bffc87381292a06c1ddbdbb8d7ffb54325cfd248
Ghozti <ybalderas1005@gmail.com>
2021-09-13 21:24:08 -0400

    - worked on Autonomous

--- core/src/robot/sim/hardware/Autonomous.java
+++ core/src/robot/sim/hardware/Autonomous.java
@@ -1,4 +1,13 @@
 package robot.sim.hardware;
 
-public class Autonomous {
+public class Autonomous extends Robot{
+
+
+    public Autonomous(String name) {
+        super(name);
+    }
+
+    public void StartAuto(){
+        //code here
+    }
 }
